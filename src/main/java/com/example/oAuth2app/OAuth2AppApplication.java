21a6d66d703e221b46dbf72dd972b1d4b0b312a0
Aviaurez <skosyrev-87@mail.ru>
2023-01-22 22:49:56 +0300

    init commit

---
+++ src/main/java/com/example/oAuth2app/OAuth2AppApplication.java
@@ -0,0 +1,13 @@
+package com.example.oAuth2app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2AppApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth2AppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/oAuth2app/config/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package com.example.oAuth2app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig{
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(a -> a
+                        .requestMatchers("/", "/error", "/webjars/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .logout(l -> l
+                        .logoutSuccessUrl("/").permitAll())
+                .csrf(c -> c
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .oauth2Login();
+
+        return http.build();
+    }
+}
